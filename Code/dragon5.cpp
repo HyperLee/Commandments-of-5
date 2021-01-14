6d1af47e73d0b1c734e63c7dde0116db0e828ce7
Peter.Chiu <tidub76217@hotmail.com>
2021-01-14 22:18:54 +0800

    Create dragon5.cpp
    Q5

---
+++ Code/dragon5.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	//unsigned int sum = 2000000000 + 2000000000;  /* ¶W¥X int ¦s©ñ½d³ò */
+    //unsigned int sum = (unsigned int) (2000000000 + 2000000000);
+    unsigned int sum = (unsigned int) 2000000000 + 2000000000;
+    
+    printf("sum:%u\n", sum);
+	
+	
+    system("pause");
+	return 0;
+}
