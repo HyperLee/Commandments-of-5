ad41d51fe6fa9c1cd7d1e25623cf6ae75fcdc164
Peter.Chiu <tidub76217@hotmail.com>
2022-02-16 22:22:37 +0800

    Update dragon5.cpp

--- Code/dragon5.cpp
+++ Code/dragon5.cpp
@@ -3,13 +3,11 @@
 
 int main()
 {
-	//unsigned int sum = 2000000000 + 2000000000;  /* 超出 int 存放範圍 */
+    //unsigned int sum = 2000000000 + 2000000000;  /* 超出 int 存放範圍 */
     //unsigned int sum = (unsigned int) (2000000000 + 2000000000);
     unsigned int sum = (unsigned int) 2000000000 + 2000000000;
     
     printf("sum:%u\n", sum);
 	
-	
-    system("pause");
 	return 0;
 }
